fd6ab2e9e89cbc8545804f007ff2192028ac9fbb
Ayush Jha <ayush@wasmer.io>
2024-05-05 17:27:46 +0545

    change theme of the terminal

--- config.def.h
+++ config.def.h
@@ -95,43 +95,39 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#3b3228", /* black   */
+  [1] = "#cb6077", /* red     */
+  [2] = "#beb55b", /* green   */
+  [3] = "#f4bc87", /* yellow  */
+  [4] = "#8ab3b5", /* blue    */
+  [5] = "#a89bb9", /* magenta */
+  [6] = "#7bbda4", /* cyan    */
+  [7] = "#d0c8c6", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#7e705a", /* black   */
+  [9]  = "#cb6077", /* red     */
+  [10] = "#beb55b", /* green   */
+  [11] = "#f4bc87", /* yellow  */
+  [12] = "#8ab3b5", /* blue    */
+  [13] = "#a89bb9", /* magenta */
+  [14] = "#7bbda4", /* cyan    */
+  [15] = "#f5eeeb", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+  /* special colors */
+  [256] = "#3b3228", /* background */
+  [257] = "#d0c8c6", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
