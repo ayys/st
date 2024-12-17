e15ec3b38d141b59cda377912d2ead0db262cc2e
Ayush Jha <ayushjha@protonmail.com>
2024-12-18 04:15:44 +0545

    chore: modify flake.nix for st

--- flake.nix
+++ flake.nix
@@ -8,29 +8,29 @@
 
   outputs =
     { self, nixpkgs, flakeutils }:
-      flakeutils.lib.eachDefaultSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          packages = rec {
-            st = pkgs.stdenv.mkDerivation {
-              name = "st";
-              src = ./.;
-              buildInputs = [
-                pkgs.gcc pkgs.pkg-config pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.ncurses pkgs.hack-font
-              ];
-              buildPhase = ''
+    flakeutils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          st = pkgs.stdenv.mkDerivation {
+            name = "st";
+            src = ./.;
+            buildInputs = [
+              pkgs.gcc pkgs.pkg-config pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.ncurses pkgs.hack-font
+            ];
+            buildPhase = ''
           make
         '';
-              installPhase = ''
+            installPhase = ''
           TERMINFO="$out" make install DESTDIR="$out" PREFIX=""
         '';
-            };
-            default = st;
           };
-          apps = rec {
-            st = flakeutils.lib.mkApp { drv = self.packages.${system}.st; };
-            default = st;
-          };
-        }
-      );
+          default = st;
+        };
+        apps = rec {
+          st = flakeutils.lib.mkApp { drv = self.packages.${system}.st; };
+          default = st;
+        };
+      }
+    );
 }
