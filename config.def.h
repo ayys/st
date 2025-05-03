56bd0a2ceea5c9e28688d32de6b0490e62209bc9
Ayush Jha <ayushjha@protonmail.com>
2025-05-03 14:50:28 +0545

    feat: use go mono font for terminal

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Victor Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Go Mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
